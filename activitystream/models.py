422af3a7c0ea59ba380e0fc9da6f472d3c782c0d
Travis Swicegood <development@domain51.com>
2009-10-16 17:02:59 -0500

    First pass at getting the models done.
    This probably isn't the final version, so don't use 'em just yet.

--- activitystream/models.py
+++ activitystream/models.py
@@ -1,2 +1,27 @@
-# Models go here
+from django.db import models
 
+class Avatar(models.Model):
+    url = models.URLField()
+
+class ActivityType(models.Model):
+    slug = models.SlugField()
+    name = models.CharField(max_length = 50)
+    source_url = models.URLField()
+    avatar = models.ForeignKey(Avatar)
+
+    def __unicode__(self):
+        return self.name
+
+class ActivityItem(models.Model):
+    uuid = models.CharField(max_length = 255)
+    slug = models.SlugField()
+    title = models.CharField(max_length = 255)
+    description = models.TextField()
+    type = models.ForeignKey(ActivityType)
+    published = models.DateTimeField()
+
+    class Meta(object):
+        ordering = ['-published']
+
+    def __unicode__(self):
+        return "%s: %s" % (self.type, self.title)
