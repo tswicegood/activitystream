1ef58a7f23c5f2134328b6074363640591e363b3
Travis Swicegood <development@domain51.com>
2009-10-16 17:03:43 -0500

    first pass at grab_activity command

---
+++ activitystream/management/commands/grab_activity.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from activitystream.models import ActivityType, ActivityItem
+import datetime
+import feedparser
+import re
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwags):
+        # TODO: handle this so it doesn't try to grab everything sequentially
+        # TODO: do this via a conditional GET
+        types = ActivityType.objects.all()
+        regex = re.compile('&[^;]+;|\(.*\)')
+        for type in types:
+            d = feedparser.parse(type.source_url)
+            for entry in d['entries']:
+                try:
+                    ActivityItem.objects.get(uuid=entry.id)
+                except ActivityItem.DoesNotExist:
+                    
+                    item = ActivityItem.objects.create(
+                        uuid=entry.id,
+                        type=type,
+                        title=entry.title,
+                        description=getattr(entry, "summary", ''),
+                        slug=regex.sub('', entry.title).strip().replace(' ', '-'),
+                        published=datetime.datetime(*entry.published_parsed[:7])
+                    )
+
