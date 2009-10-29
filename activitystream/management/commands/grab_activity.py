2b46d2576808890039527e046d08ed568306bbf2
Travis Swicegood <development@domain51.com>
2009-10-29 11:29:14 -0500

    add some code that's necessary to grab the elements from Vimeo

--- activitystream/management/commands/grab_activity.py
+++ activitystream/management/commands/grab_activity.py
@@ -4,6 +4,14 @@ import datetime
 import feedparser
 import re
 
+def add_extra(parent, data):
+    return Link.objects.create(
+        parent=parent,
+        href=data["href"],
+        rel="rel" in data and data["rel"] or "",
+        type=data["type"]
+    )
+
 class Command(BaseCommand):
     def handle(self, *args, **kwags):
         # TODO: handle this so it doesn't try to grab everything sequentially
@@ -16,6 +24,7 @@ class Command(BaseCommand):
                 try:
                     ActivityItem.objects.get(uuid=entry.id)
                 except ActivityItem.DoesNotExist:
+                    published = getattr(entry, "published_parsed", entry.updated_parsed)
                     item = ActivityItem.objects.create(
                         uuid=entry.id,
                         source_url=entry.link,
@@ -23,13 +32,10 @@ class Command(BaseCommand):
                         title=entry.title,
                         description=getattr(entry, "summary", ''),
                         slug=regex.sub('', entry.title).strip().replace(' ', '-'),
-                        published=datetime.datetime(*entry.published_parsed[:7])
+                        published=datetime.datetime(*published)[:7]
                     )
-                    for link in entry['links']:
-                        Link.objects.create(
-                            parent=item,
-                            href=link['href'],
-                            rel=link['rel'],
-                            type=link['type']
-                        )
+                    if "links" in entry:
+                        [add_extra(item, a) for a in entry["links"]]
+                    if "enclosures" in entry:
+                        [add_extra(item, a) for a in entry["enclosures"]]
 
