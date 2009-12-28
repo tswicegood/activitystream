4e316c218fa15b7d975d941443ec571ca021e947
Chris Dickinson <christopher.s.dickinson@gmail.com>
2009-12-28 11:22:11 -0600

    added simple view to check that activityitems are being grouped as expected

--- activitystream/urls.py
+++ activitystream/urls.py
@@ -1,9 +1,12 @@
-from activitystream.models import ActivityItem
+from activitystream.models import ItemGroup, ActivityItem 
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
     url(r'^list/?$', 'django.views.generic.list_detail.object_list', {
         'queryset': ActivityItem.objects.all(),
     }, name='activitystream_list'),
+    url(r'^collapsed/?$', 'django.views.generic.list_detail.object_list', {
+        'queryset': ItemGroup.objects.all(),
+    }, name='activitystream_list_collapsed')
 )
 
